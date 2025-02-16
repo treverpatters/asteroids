d3f7e4096cafca4b6f6f6b86c1d5645cb64ab447
treverleach19 <treverleach19@gmail.com>
2025-02-16 13:41:26 -0600

    C: added left and right turning

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
             
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         #limit the framerate to 60 FPS
